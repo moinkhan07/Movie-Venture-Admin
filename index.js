c82c2d6c8fc4c6cadf1e452583206821a1c85b6e
moinkhan07 <moin6k@gmail.com>
2024-01-24 12:54:36 +0530

    updated index page

--- index.js
+++ index.js
@@ -3,27 +3,9 @@ const adminlogin = async ()=>{
         adminEmail : document.getElementById("loginemail").value,
         adminPassword: document.getElementById("loginpass").value,
     }
-    if(adminCredentials.adminEmail == ""){
-        window.alert("Please enter your email!")
-        return;
-    }else if(adminCredentials.adminPassword == ""){
-        window.alert("Please enter your password!")
-        return;
+    if(adminCredentials.adminEmail == "admin" && adminCredentials.adminPassword == "admin"){
+      window.alert("Welcome Back,Admin!");
     }
-    let res = await fetch("http://movieventurewebapp.eu-north-1.elasticbeanstalk.com/adminlogin", {
-        method: "POST",
-        body: JSON.stringify(adminCredentials),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      let data = await res.text();
-      if(data == "Admin Successfully Logged In"){
-        window.alert("Welcome to Movie Venture!")
-        window.location.href = "./adminpanel.html"
-      }else{
-        window.alert("Email or Password is Incorrect!")
-      }
      document.getElementById("loginemail").value ="";
      document.getElementById("loginpass").value = "";
 }
